cf87f8141a73f9f72b623e6d1de89e06a7be4fa9
asbohaska <asbohaska@gmail.com>
2021-06-13 11:20:46 -0400

    Getting Ready to Build

--- ConCatEnate/Assets/Scripts/ButtonSound.cs
+++ ConCatEnate/Assets/Scripts/ButtonSound.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class ButtonSound : MonoBehaviour
 {
+public static ButtonSound Instance {get; private set; }
+
+private void Awake () {
+    //Initiate singleton
+	if (Instance == null) {
+		Instance = this;
+		DontDestroyOnLoad(gameObject);
+	} else {
+		Destroy(gameObject);
+	}
+}
 public AudioSource audioSource;
 public AudioClip audioClip;
  
