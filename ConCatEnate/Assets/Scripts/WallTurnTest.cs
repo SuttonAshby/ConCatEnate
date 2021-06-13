00d1a0e0f6d9c36e82d9c032cc9131d632a7e03b
asbohaska <asbohaska@gmail.com>
2021-06-13 02:47:25 -0400

    Wall Script and Updated Rules Added

---
+++ ConCatEnate/Assets/Scripts/WallTurnTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallTurnTest : MonoBehaviour
+{
+void OnControllerColliderHit ( ControllerColliderHit hit ) {
+ 
+        if (hit.collider.gameObject.tag == "Wall"){
+         
+            Vector3 eulerAngles = transform.rotation.eulerAngles;          
+ 
+            // Set the altered rotation back
+            transform.rotation = Quaternion.AngleAxis(90, transform.up) * transform.rotation;       
+ 
+        Debug.Log ("HIT WALL - ROTATING!"); // Display it in UI
+        }
+}
+}
